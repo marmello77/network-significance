944c0d0500df89f19f83a3dcc4b842c6e16e136c
Marco Mello <marmello@gmail.com>
2018-11-14 10:40:37 -0200

    Update network-significance.R

--- network-significance.R
+++ network-significance.R
@@ -6,28 +6,13 @@
 
 ##### Ecological Synthesis Lab (SintECO)
 ##### https://marcomellolab.wordpress.com
-##### Authors: Renata Muylaert & Pavel Dodonov
+##### Authors: Renata Muylaert, Pavel Dodonov & Marco Mello
 ##### E-mail: renatamuy@gmail.com
-##### How to cite: Muylaert RL & Dodonov P. 2016. How to estimate
-##### P-values of network metrics and compare pairs of networks
-##### using Monte Carlo procedures. Ecological Synthesis Lab
-##### at the University of São Paulo, Brazil.
-
+##### How to cite: Muylaert RL, Dodonov P & Marco Mello. 2016. How to estimate P-values of network metrics and compare pairs of networks using Monte Carlo procedures. Ecological Synthesis Lab at the University of São Paulo, Brazil.
 ##### Published on April 25th, 2017 (English version).
 ##### Run in R 3.3.3 (2017-03-06) -- "Another Canoe"
 
-##### Disclaimer: You may use this script freely for non-comercial
-##### purposes at your own risk. We assume no responsibility or
-##### liability for the use of this software, convey no license
-##### or title under any patent, copyright, or mask work right
-##### to the product. We reserve the right to make changes in
-##### the software without notification. 
-##### We also make no representation or warranty that such
-##### application will be suitable for the specified use without
-##### further testing or modification. If this script helps you
-##### produce any academic work (paper, book, chapter, 
-##### dissertation etc.), please acknowledge the authors and
-##### cite the source.
+##### Disclaimer: You may use this script freely for non-comercial purposes at your own risk. We assume no responsibility or liability for the use of this software, convey no license or title under any patent, copyright, or mask work right to the product. We reserve the right to make changes in the software without notification. We also make no representation or warranty that such application will be suitable for the specified use without further testing or modification. If this script helps you produce any academic work (paper, book, chapter, dissertation etc.), please acknowledge the authors and cite the source.
 
 
 #############################################################
